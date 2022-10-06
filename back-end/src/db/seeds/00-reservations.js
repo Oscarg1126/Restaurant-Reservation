fede7a6ba2a8fc46d74d8e1dc56e96568c2b0f2e
Oscar Gurrola <oscarg20rm@gmail.com>
2022-10-06 18:36:59 -0500

    updated files

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,7 +1,9 @@
-const reservations = require("./00-reservations.json");
+const reservations = require("../seeds/00-reservations.json");
 
 exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
-    .then(() => knex("reservations").insert(reservations));
+return knex
+.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+.then(() => {
+return knex("reservations").insert(reservations)
+})
 };
